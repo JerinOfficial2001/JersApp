b1854b823cd9144db668263729c00091ed20a6cd
JerinOfficial2001 <jerinjerijree@gmail.com>
2024-01-19 18:36:33 +0530

    cors

--- watsappcloneapi/pages/api/auth/login.js
+++ watsappcloneapi/pages/api/auth/login.js
@@ -1,70 +1,82 @@
 import connectToDatabase from '@/api/lib/db';
 import Auth from '@/api/model/auth';
 import jwt from 'jsonwebtoken';
+import cors from 'cors';
 
 const SECRET_KEY = process.env.NEXT_PUBLIC_SECRET_KEY; // Replace with the same secret key used for signing tokens
+const corsMiddleware = cors({
+  origin: ['http://localhost:3001', 'https://next-api-ruby.vercel.app'], // Replace with the actual origin of your client app
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+});
 
 export default async function handler(req, res) {
   await connectToDatabase();
-  const {method} = req;
 
-  switch (method) {
-    case 'POST':
-      try {
-        const {mobNum, password} = req.body;
-        const user = await Auth.findOne({mobNum});
+  // Use cors middleware by including headers in the response
+  corsMiddleware(req, res, async () => {
+    const {method} = req;
 
-        if (user.password === password) {
-          // Generate JWT token
-          const token = jwt.sign({mobNum: user.mobNum}, SECRET_KEY, {
-            expiresIn: '1h',
-          });
+    switch (method) {
+      case 'POST':
+        try {
+          const {mobNum, password} = req.body;
+          const user = await Auth.findOne({mobNum});
 
-          res.status(200).json({status: 'ok', data: {token}});
-        } else {
-          res.status(401).json({status: 'error', data: 'Invalid credentials'});
-        }
-      } catch (error) {
-        console.error('Error:', error);
-        res
-          .status(401)
-          .json({status: 'error', data: `Unauthorized - ${error.message}`});
-      }
+          if (user.password === password) {
+            // Generate JWT token
+            const token = jwt.sign({mobNum: user.mobNum}, SECRET_KEY, {
+              expiresIn: '1h',
+            });
 
-      break;
-
-    case 'GET':
-      try {
-        // Extract token from the request headers or cookies
-        const token = req.headers.authorization?.replace('Bearer ', ''); // Adjust this according to your token handling
-
-        if (!token) {
-          return res
+            res.status(200).json({status: 'ok', data: {token}});
+          } else {
+            res
+              .status(401)
+              .json({status: 'error', data: 'Invalid credentials'});
+          }
+        } catch (error) {
+          console.error('Error:', error);
+          res
             .status(401)
-            .json({status: 'error', data: 'Unauthorized - Missing Token'});
+            .json({status: 'error', data: `Unauthorized - ${error.message}`});
         }
 
-        // Verify and decode the token
-        const decoded = jwt.verify(token, SECRET_KEY);
+        break;
 
-        // Retrieve user data based on the decoded information
-        const user = await Auth.findById(decoded.userId);
+      case 'GET':
+        try {
+          // Extract token from the request headers or cookies
+          const token = req.headers.authorization?.replace('Bearer ', ''); // Adjust this according to your token handling
 
-        if (user) {
-          res.status(200).json({status: 'ok', data: {user}});
-        } else {
-          res.status(404).json({status: 'error', data: 'User not found'});
+          if (!token) {
+            return res
+              .status(401)
+              .json({status: 'error', data: 'Unauthorized - Missing Token'});
+          }
+
+          // Verify and decode the token
+          const decoded = jwt.verify(token, SECRET_KEY);
+
+          // Retrieve user data based on the decoded information
+          const user = await Auth.findById(decoded.userId);
+
+          if (user) {
+            res.status(200).json({status: 'ok', data: {user}});
+          } else {
+            res.status(404).json({status: 'error', data: 'User not found'});
+          }
+        } catch (error) {
+          console.error('Error:', error);
+          res
+            .status(401)
+            .json({status: 'error', data: 'Unauthorized - Invalid Token'});
         }
-      } catch (error) {
-        console.error('Error:', error);
-        res
-          .status(401)
-          .json({status: 'error', data: 'Unauthorized - Invalid Token'});
-      }
-      break;
+        break;
 
-    default:
-      res.status(405).json({error: 'Method Not Allowed'});
-      break;
-  }
+      default:
+        res.status(405).json({error: 'Method Not Allowed'});
+        break;
+    }
+  });
 }
