0db8c79b6b9d41f60e3642eb09928805c3b566a1
jerin.t <jerin.t@iprotecs.net>
2024-01-31 17:33:17 +0530

    DB url changed

--- watsappcloneapi/socket/index.js
+++ watsappcloneapi/socket/index.js
@@ -13,9 +13,9 @@ const io = require('socket.io')(4000, {
   },
 });
 io.on('connection', socket => {
-  console.log('connected');
+  console.log('User connected');
   socket.on('disconnect', () => {
-    console.log('Disconnected');
+    console.log('User Disconnected');
   });
   socket.on('message', async obj => {
     await Message.create(obj);
