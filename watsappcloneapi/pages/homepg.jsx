ed1973375a38fe5524c53ac35567e15bd56affed
jerin.t <jerin.t@iprotecs.net>
2024-02-06 15:55:20 +0530

    local socket server check

--- watsappcloneapi/pages/homepg.jsx
+++ watsappcloneapi/pages/homepg.jsx
@@ -26,7 +26,7 @@ export default function Homepg() {
   const [chatArray, setchatArray] = useState([]);
   const [currentChatPg, setcurrentChatPg] = useState({});
   const handleSocket = async () => {
-    const socketData = io(SocketAPI);
+    const socketData = io("http://localhost:4000");
     if (socketData) {
       setsocket(socketData);
     }
