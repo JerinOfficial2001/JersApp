9e4d763d984e31e08b8011672c6c6cc55c0e2051
Jerin <78803776+JerinOfficial2001@users.noreply.github.com>
2024-02-23 15:13:52 +0530

    Update homepg.jsx

--- watsappcloneapi/pages/homepg.jsx
+++ watsappcloneapi/pages/homepg.jsx
@@ -1,61 +1,58 @@
-import {getAllUsers} from '@/api/controller/auth';
-import {createChat, getAllChats} from '@/api/controller/chats';
-import {getAllMessages} from '@/api/controller/message';
-import {Box} from '@mui/material';
-import Cookies from 'js-cookie';
-import {useRouter} from 'next/router';
-import React, {useEffect, useState} from 'react';
-import { io } from 'socket.io-client';
-
+import { getAllUsers } from "@/api/controller/auth";
+import { createChat, getAllChats } from "@/api/controller/chats";
+import { getAllMessages } from "@/api/controller/message";
+import { Box } from "@mui/material";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
+import { io } from "socket.io-client";
 
 export default function Homepg() {
   const router = useRouter();
   const [users, setusers] = useState([]);
   const [userData, setuserData] = useState({});
   const [formDatas, setformDatas] = useState({
-    msg: '',
-    userName: '',
+    msg: "",
+    userName: "",
   });
-  const handleOnchange = event => {
-    const {value, name} = event.target;
+  const handleOnchange = (event) => {
+    const { value, name } = event.target;
     setformDatas({
       ...formDatas,
       [name]: value,
     });
   };
- const token = Cookies.get('token')
+  const token = Cookies.get("token");
 
   const SocketAPI = process.env.NEXT_PUBLIC_SOCKET_API;
-  const [socket, setsocket] = useState(null);
+  // const [socket, setsocket] = useState(null);
   const [chatArray, setchatArray] = useState([]);
   const [currentChatPg, setcurrentChatPg] = useState({});
-  const socketData = io(SocketAPI, {
-    path: '/socket',
-    reconnection: true,
-    transports: ['websocket', 'polling'],
-    reconnectionAttempts:5
+  const socket = io(SocketAPI, {
+    path: "/socket",
+    // reconnection: true,
+    transports: ["websocket", "polling"],
+    // reconnectionAttempts:5
   });
-  useEffect(() => {
-    if (socketData && token) {
-      setsocket(socketData);
-     
-      return () => {
-      socketData.disconnect();
-      console.log('Disconnected from Socket.IO server');
-    };
-    }
+  // useEffect(() => {
+  //   if (socketData && token) {
+  //     setsocket(socketData);
 
-    // Clean up the socket connection when the component unmounts
-    
-  }, [])
- 
+  //     return () => {
+  //     socketData.disconnect();
+  //     console.log('Disconnected from Socket.IO server');
+  //   };
+  //   }
+
+  //   // Clean up the socket connection when the component unmounts
+
+  // }, [])
 
   const handleSocket = async () => {
-   
     if (socket) {
-      socket.on('message', data => {
+      socket.on("message", (data) => {
         if (chatID && data) {
-          const filteredMsg = data.filter(msg => msg.chatID == chatID);
+          const filteredMsg = data.filter((msg) => msg.chatID == chatID);
           if (filteredMsg) {
             setchatArray(filteredMsg);
           }
@@ -63,8 +60,8 @@ export default function Homepg() {
       });
     }
   };
-  const getUsers = data => {
-    getAllUsers(data._id).then(res => {
+  const getUsers = (data) => {
+    getAllUsers(data._id).then((res) => {
       if (res) {
         setusers(res);
       }
@@ -72,41 +69,40 @@ export default function Homepg() {
   };
   useEffect(() => {
     handleSocket();
-    const storageData = localStorage.getItem('userData');
+    const storageData = localStorage.getItem("userData");
     if (storageData) {
       const userDetails = JSON.parse(storageData);
       setuserData(userDetails);
       getUsers(userDetails);
     }
-   
   }, []);
-  const [chatID, setchatID] = useState('');
+  const [chatID, setchatID] = useState("");
 
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if (formDatas.msg !== '') {
-      socket.emit('message', {
+    if (formDatas.msg !== "") {
+      socket.emit("message", {
         chatID: chatID,
         sender: userData.name,
         receiver: currentChatPg?.receiver,
         message: formDatas.msg,
       });
       setformDatas({
-        msg: '',
-        userName: '',
+        msg: "",
+        userName: "",
       });
       handleSocket();
     }
   };
-  const addChat = data => {
+  const addChat = (data) => {
     if (data.sender && data.receiver) {
       createChat(data);
       setcurrentChatPg(data);
       handleSocket();
-      getAllChats(data.sender, data.receiver).then(chat => {
+      getAllChats(data.sender, data.receiver).then((chat) => {
         if (chat) {
           setchatID(chat._id);
-          getAllMessages(chat._id).then(msg => {
+          getAllMessages(chat._id).then((msg) => {
             if (msg) {
               setchatArray(msg);
             }
@@ -115,64 +111,68 @@ export default function Homepg() {
       });
     }
   };
-  const getUserNameByID = id => {
-    const res = users.find(user => user._id === id);
+  const getUserNameByID = (id) => {
+    const res = users.find((user) => user._id === id);
     if (res) {
       return res?.name;
     } else {
-      return 'Name';
+      return "Name";
     }
   };
   return (
     <div
       style={{
-        height: '100vh',
-        width: '100%',
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        flexDirection: 'row',
+        height: "100vh",
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        flexDirection: "row",
         gap: 3,
         padding: 5,
-      }}>
+      }}
+    >
       <button
         onClick={() => {
           localStorage.clear();
-          router.push('/');
-        }}>
+          router.push("/");
+        }}
+      >
         Logout
       </button>
       <div
         style={{
-          width: '40%',
-          height: '400px',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'column',
+          width: "40%",
+          height: "400px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
           gap: 3,
-          background: 'white',
-        }}>
-        <h3 style={{color: 'black'}}>{userData?.name}</h3>
+          background: "white",
+        }}
+      >
+        <h3 style={{ color: "black" }}>{userData?.name}</h3>
         {users?.map((elem, index) => {
           return (
             <div
               onClick={() => {
-                addChat({sender: userData._id, receiver: elem._id});
+                addChat({ sender: userData._id, receiver: elem._id });
               }}
               key={index}
               style={{
-                height: '60px',
-                width: '100%',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                flexDirection: 'column',
+                height: "60px",
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
                 gap: 3,
-                borderRadius: '10px',
-                border: '1px solid green',
-                color: 'black',
-              }}>
+                borderRadius: "10px",
+                border: "1px solid green",
+                color: "black",
+              }}
+            >
               <p>{elem.name}</p>
             </div>
           );
@@ -180,75 +180,80 @@ export default function Homepg() {
       </div>
       <div
         style={{
-          height: '100vh',
-          width: '60%',
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          flexDirection: 'column',
+          height: "100vh",
+          width: "60%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexDirection: "column",
           gap: 3,
-          background: 'gray',
-        }}>
+          background: "gray",
+        }}
+      >
         <h1>{getUserNameByID(currentChatPg?.receiver)}</h1>
         <div
           style={{
-            minHeight: '90vh',
-            width: '90%',
+            minHeight: "90vh",
+            width: "90%",
             gap: 3,
-            background: 'slategray',
+            background: "slategray",
             padding: 5,
             paddingBottom: 70,
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'flex-end',
-          }}>
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-end",
+          }}
+        >
           <Box
             sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              overflow: 'hidden',
-              '&:hover': {
-                overflowY: 'auto', // Show overflow when hovering over the side menu
+              display: "flex",
+              flexDirection: "column",
+              overflow: "hidden",
+              "&:hover": {
+                overflowY: "auto", // Show overflow when hovering over the side menu
               },
-              '&::-webkit-scrollbar': {
-                width: '8px',
+              "&::-webkit-scrollbar": {
+                width: "8px",
               },
-              '&::-webkit-scrollbar-track': {
-                background: '#f5f5f5',
-                borderRadius: '4px',
+              "&::-webkit-scrollbar-track": {
+                background: "#f5f5f5",
+                borderRadius: "4px",
               },
-              '&::-webkit-scrollbar-thumb': {
-                background: '#bdbdbd',
-                borderRadius: '4px',
-                '&:hover': {
-                  background: '#a5a5a5',
+              "&::-webkit-scrollbar-thumb": {
+                background: "#bdbdbd",
+                borderRadius: "4px",
+                "&:hover": {
+                  background: "#a5a5a5",
                 },
               },
-            }}>
+            }}
+          >
             {chatArray.map((chat, index) => {
               const isCurrentUser = chat.sender === userData.name;
               return (
                 <div
                   key={index}
                   style={{
-                    height: 'auto',
-                    width: '100%',
-                    display: 'flex',
-                    justifyContent: isCurrentUser ? 'flex-end' : 'flex-start',
-                    alignItems: 'center',
+                    height: "auto",
+                    width: "100%",
+                    display: "flex",
+                    justifyContent: isCurrentUser ? "flex-end" : "flex-start",
+                    alignItems: "center",
                     padding: 3,
                     gap: 3,
-                  }}>
+                  }}
+                >
                   <p
                     style={{
                       borderRadius: isCurrentUser
-                        ? '10px 10px 0 10px'
-                        : '0 10px 10px 10px',
-                      border: '1px solid black',
-                      color: 'black',
-                      background: 'white',
+                        ? "10px 10px 0 10px"
+                        : "0 10px 10px 10px",
+                      border: "1px solid black",
+                      color: "black",
+                      background: "white",
                       padding: 10,
-                    }}>
+                    }}
+                  >
                     {chat.message}
                   </p>
                 </div>
@@ -257,7 +262,7 @@ export default function Homepg() {
           </Box>
         </div>
 
-        <form style={{position: 'fixed', bottom: 10}} onSubmit={handleSubmit}>
+        <form style={{ position: "fixed", bottom: 10 }} onSubmit={handleSubmit}>
           <input
             type="text"
             value={formDatas.msg}
