246eb9a6bee3c68887c43de20425f996e7ea7cc6
jerin.t <jerin.t@iprotecs.net>
2024-07-09 19:16:40 +0530

    Group notification

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 export const myLapIP = 'http://192.168.53.174:4000';
 export const nextApi = 'https://real-time-chat-next-api.vercel.app';
-// export const socketServerApi = 'https://socket-server-fhra.onrender.com';
+export const socketServerApi = 'https://socket-server-fhra.onrender.com';
 export const expressApi = 'https://whatsapp-api-wine.vercel.app';
-export const socketServerApi = 'http://10.60.36.87:4000';
+// export const socketServerApi = 'http://10.60.36.87:4000';
 // export const socketServerApi = 'http://192.168.186.174:4000';
 // export const expressApi = 'http://10.60.36.87:8080';
 // export const expressApi = 'http://192.168.186.174:8080';
